6c900679a5ebf74aacafb4af3f1a5287a2ecd19c
Marco Langer <langer.m86@gmail.com>
2021-10-26 20:50:11 +0200

    style

--- include/cpp_utils/algorithm.hpp
+++ include/cpp_utils/algorithm.hpp
@@ -6,34 +6,30 @@
 
 namespace ml {
 
+// TODO constraints
+
 template <typename Head0, typename Head1, typename... Tail>
 constexpr auto
-max(
-        Head0     && head0,
-        Head1     && head1,
-        Tail      &&... tail) {
-
-    auto result = head0 < head1 ? head1 : head0;
-    auto max_helper = [&result](auto && el) {
-      result = (el < result) ? result : el;
-    };
-    (..., max_helper(std::forward<Tail>(tail)));
-    return result;
+max(Head0 && head0, Head1 && head1, Tail &&... tail)
+{
+  auto result = head0 < head1 ? head1 : head0;
+  auto max_helper = [&result](auto && el) {
+    result = (el < result) ? result : el;
+  };
+  (..., max_helper(std::forward<Tail>(tail)));
+  return result;
 }
 
 template <typename Head0, typename Head1, typename... Tail>
 constexpr auto
-min(
-        Head0     && head0,
-        Head1     && head1,
-        Tail      &&... tail) {
-
-    auto result = head0 < head1 ? head0 : head1;
-    auto max_helper = [&result](auto && el) {
-      result = (el < result) ? el : result;
-    };
-    (..., max_helper(std::forward<Tail>(tail)));
-    return result;
+min(Head0 && head0, Head1 && head1, Tail &&... tail)
+{
+  auto result = head0 < head1 ? head0 : head1;
+  auto max_helper = [&result](auto && el) {
+    result = (el < result) ? el : result;
+  };
+  (..., max_helper(std::forward<Tail>(tail)));
+  return result;
 }
 
 } // namespace ml
