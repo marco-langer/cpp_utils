88ee5a2454175eeb8f2412b2b64e1980d24c2115
Marco Langer <langer.m86@gmail.com>
2022-01-09 21:06:17 +0100

    tuple_for_each refactoring

--- include/cpp_utils/tuple_utils.hpp
+++ include/cpp_utils/tuple_utils.hpp
@@ -15,14 +15,15 @@ struct is_tuple<std::tuple<Ts...>> : std::true_type {};
 template <typename T>
 inline constexpr bool is_tuple_v = is_tuple<T>::value;
 
-template <typename Func, typename... Ts>
+template <typename Tuple, typename Func>
 void
-tuple_for_each(const std::tuple<Ts...> tp, Func f)
+tuple_for_each(Tuple && tp, Func && f)
 {
-  std::apply([&](auto && ... args)
-    {
-      (std::forward<Func>(f)(args), ...);
-    }, tp
+  std::apply(
+    [&](auto && ... args) {
+      (std::forward<Func>(f)(std::forward<decltype(args)>(args)), ...);
+    },
+    std::forward<Tuple>(tp)
   );
 }
 
